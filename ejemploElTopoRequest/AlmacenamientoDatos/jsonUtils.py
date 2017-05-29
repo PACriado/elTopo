8c3ce088ccaed15ebca24e2fbf34aeb7f101c4bd
pabloabel <pabloabel.criado@hotmail.com>
2017-05-29 22:34:58 +0200

    Añadida conprobacion de la existencia de la ruta donde se guardan los ficheros. Si la ruta no existe, se crea

--- ejemploElTopoRequest/AlmacenamientoDatos/jsonUtils.py
+++ ejemploElTopoRequest/AlmacenamientoDatos/jsonUtils.py
@@ -1,11 +1,13 @@
 import time
 import re
+import os
 
 class jsonUtil:
     def __init__(self, baseDirectory):
         self.baseDirectory = baseDirectory
 
     def escribirJson(self,contenido,fileName, format):
+        if not os.path.exists(self.baseDirectory): os.makedirs(self.baseDirectory)#con esto nos aseguramos que la ruta donde guardamos los datos existe
         outfile = open(self.baseDirectory+ self.nombreFichero(fileName)+format, 'w')
         outfile.write(contenido)
         outfile.close()
