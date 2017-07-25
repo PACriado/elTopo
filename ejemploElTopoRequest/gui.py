15d87ec227b098caa579a8300619c85e543bc083
pabloabel <pabloabel.criado@hotmail.com>
2017-07-25 22:18:30 +0200

    Limpieza de codigo del ejemplo

---
+++ ejemploElTopoRequest/gui.py
@@ -0,0 +1,7 @@
+from interfazGrafica.iConfiguracion import Ventana
+
+def main():
+    app = Ventana()
+
+if __name__ == '__main__':
+    main()
--- ejemploElTopoRequest/interfazGrafica/iConfiguracion.py
+++ ejemploElTopoRequest/interfazGrafica/iConfiguracion.py
@@ -1,89 +1,12 @@
 from tkinter import *
 from tkinter import ttk
-import json
-
-
-class config:
-
-    def __init__(self, route=''):
-        if route != '':
-            #DE ESTA FORMA CARGAMOS UN JSON A ESTA CLASE
-            with open(route) as json_data:
-                self.__dict__ = json.load(json_data)
-        else:
-            self.RutaSalida =""
-            self.RutaDiccionario=""
-            self.MaxDepth=1
-            self.UsarDiccionario= False
-            self.UsarSiempreTor=False
-            self.RenovarSiempreCircuitoTor=False
-            self.DelayIntentoRenovacionCircuitoTor=2
-            self.url=[]
-
-
-    def setRutaSalida(self, rutaSalida):
-        self.RutaSalida = rutaSalida
-
-    def getRutaSalida(self):
-        return self.RutaSalida
-
-    def setRutaDiccionario(self, rutaDiccionario):
-        self.RutaDiccionario = rutaDiccionario
-
-    def getRutaDiccionario(self):
-        return self.RutaDiccionario
-
-    def setMaxDepth(self, maxDepth):
-        self.MaxDepth = maxDepth
-
-    def getMaxDepth(self):
-        return self.MaxDepth
-
-    def setUsarDiccionario(self, usarDiccionario):
-        self.UsarDiccionario = usarDiccionario
-
-    def getUsarDiccionario(self):
-        return self.UsarDiccionario
-
-    def setUsarSiempreTor(self, usarSiempreTor):
-        self.UsarSiempreTor = usarSiempreTor
-
-    def getUsarSiempreTor(self):
-        return self.UsarSiempreTor
-
-    def setRenovarSiempreCircuitoTor(self, renovarSiempreCircuitoTor):
-        self.RenovarSiempreCircuitoTor = renovarSiempreCircuitoTor
-
-    def getRenovarSiempreCircuitoTor(self):
-        return self.RenovarSiempreCircuitoTor
-
-    def setDelayIntentoRenovacionCircuitoTor(self, delayIntentoRenovacionCircuitoTor):
-        self.DelayIntentoRenovacionCircuitoTor = delayIntentoRenovacionCircuitoTor
-
-    def getDelayIntentoRenovacionCircuitoTor(self):
-        return self.DelayIntentoRenovacionCircuitoTor
-
-    def seturl(self, Url):
-        self.url = Url
-
-    def geturl(self):
-        return self.url
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-
-    def writeFileJSON(self,file):
-        outfile = open(file, 'w')
-        outfile.write(self.toJSON())
-        outfile.close()
-
-
-
-
-
+from configElTopo.config import config
 
 class Ventana():
     def __init__(self):
+        self.rutaConfig= "./configElTopo/config.json"
+        self.configuracion = config(route=self.rutaConfig)
+
         self.ventana = Tk()
         self.ventana.geometry('300x200')
         self.ventana.title('Prueba3')
@@ -94,7 +17,6 @@ class Ventana():
         self.botoninfo.pack(side = LEFT)
         self.botonsalir =ttk.Button(self.ventana, text='Salir',command=self.ventana.destroy)
         self.botonsalir.pack(side=RIGHT)
-        self.conf = config()
         #Resaltamos el borde del botoninfo
         self.botoninfo.focus_set()
         self.ventana.mainloop()
@@ -111,15 +33,3 @@ class Ventana():
         self.tinfo.insert("1.0",info)
 
 
-
-
-
-
-
-def main():
-    app = Ventana()
-
-
-
-if __name__ == '__main__':
-    main()
