f6cb23f957c37a7dd836a8970db66dcd7935e912
rebeca <rebecaglez22@gmail.com>
2017-07-29 05:23:34 +0200

    Cambios en la ventana de configuracion: Checkbox por Combobox, funcionando.

--- ejemploElTopoRequest/interfazGrafica/VentanaConfig.py
+++ ejemploElTopoRequest/interfazGrafica/VentanaConfig.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk,font
+from tkinter import ttk
 from configElTopo.config import config
 
 
@@ -41,32 +41,46 @@ class VentanaConfig():
         #row 3 : Tercera fila
         self.LBLMaxDepth = ttk.Label(self.marco,text="Maxdepth:")
         self.LBLMaxDepth.grid(row=2,column=0,sticky=W)
-      #  maxD_str = self.configuracion.getMaxDepth()
-       # self.TXTMaxDepth = ttk.Entry(self.marco,width=30)
-      #  self.TXTMaxDepth.insert(0,maxD_str)
-      #  self.TXTMaxDepth.grid(row=2,column=1)
+        maxD_str = self.configuracion.getMaxDepth()
+        #self.TXTMaxDepth = ttk.Entry(self.marco,width=30)
+        #self.TXTMaxDepth.insert(0,maxD_str)
+        #self.TXTMaxDepth.grid(row=2,column=1)
+        #self.SCLMaxDepth = Scale(self.marco, from_=1, to=200, orient=HORIZONTAL)
+        #self.SCLMaxDepth.grid(row=2,column=1,sticky=W)
 
-      #  self.SCLMaxDepth = Scale(self.marco, from_=1, to=200, orient=HORIZONTAL)
-     #   self.SCLMaxDepth.grid(row=2,column=1,sticky=W)
-
-        self.SBOXMaxDepth = Spinbox(self.marco, from_=0, to=10)
+        self.SBOXMaxDepth = Spinbox(self.marco, from_=maxD_str, to=10)
         self.SBOXMaxDepth.grid(row=2,column=1,sticky=W)
 
 
         #row 4 BOOL
         self.LBLUsarDic = ttk.Label(self.marco,text="Usar Diccionario:")
         self.LBLUsarDic.grid(row=3,column=0,sticky=W)
-       # usarD_str = self.configuracion.getUsarDiccionario()
-       # self.TXTUsarDic = ttk.Entry(self.marco,width=30)
-       # self.TXTUsarDic.insert(0,usarD_str)
-        self.var= self.configuracion.getUsarDiccionario()
+        #usarD_str = self.configuracion.getUsarDiccionario()
+        #self.TXTUsarDic = ttk.Entry(self.marco,width=30)
+        #self.TXTUsarDic.insert(0,usarD_str)
+        self.usarD_str=self.configuracion.getUsarDiccionario()
+        #self.int_var = int(self.var)
+        #print(self.var)
+        #print(self.int_var)
+        print(self.usarD_str)
+        print(int(self.usarD_str))
 
-       # self.checkbox_value = BooleanVar(self)
 
-        prueba=int(self.var=='True')
+        value=StringVar(self.usarD_str)
 
-        self.chk1 = Checkbutton(self.marco,variable=prueba)
-        self.chk1.grid(row=3,column=1,sticky=W)
+
+        self.CMBUsarDic = ttk.Combobox(self.marco,textvariable=value,state ='readonly')
+        self.CMBUsarDic['values'] = ['False', 'True']
+        self.CMBUsarDic.current(int(self.usarD_str))
+        self.CMBUsarDic.grid(row=3,column=1,sticky=W)
+
+
+        #self.checkbox_value = BooleanVar(self)
+
+        #prueba=int(self.var=='True')
+
+        #self.chk1 = Checkbutton(self.marco,variable=prueba)
+        #self.chk1.grid(row=3,column=1,sticky=W)
 
 
         #self.TXTUsarDic.grid(row=3,column=1)
@@ -75,9 +89,19 @@ class VentanaConfig():
         self.LBLUsarTor = ttk.Label(self.marco,text="Usar Tor:")
         self.LBLUsarTor.grid(row=4,column=0,sticky=W)
         self.usarT_str= self.configuracion.getUsarSiempreTor()
+        print(self.usarT_str)
 
-        self.chk2 = Checkbutton(self.marco,variable=self.usarT_str)
-        self.chk2.grid(row=4,column=1,sticky=W)
+        # self.chk2 = Checkbutton(self.marco,variable=self.usarT_str)
+
+        value=StringVar(self.usarT_str)
+
+        self.CMBUsarTor = ttk.Combobox(self.marco,textvariable=value,state ='readonly')
+        self.CMBUsarTor['values'] = ['False', 'True']
+        self.CMBUsarTor.current(int(self.usarT_str))
+        self.CMBUsarTor.grid(row=4,column=1,sticky=W)
+
+
+        #self.chk2.grid(row=4,column=1,sticky=W)
 
         #self.TXTUsarTor = ttk.Entry(self.marco,width=30)
         #self.TXTUsarTor.insert(0,usarT_str)
@@ -86,10 +110,22 @@ class VentanaConfig():
         #row 6 BOOL
         self.LBLRenovarC= ttk.Label(self.marco,text="Renovar circuito siempre:")
         self.LBLRenovarC.grid(row=5,column=0,sticky=W)
-       # renovarC_str = self.configuracion.getRenovarSiempreCircuitoTor()
+        #renovarC_str = self.configuracion.getRenovarSiempreCircuitoTor()
         self.renovarC_str= self.configuracion.getRenovarSiempreCircuitoTor()
-        self.chk3 = Checkbutton(self.marco,variable=self.renovarC_str)
-        self.chk3.grid(row=5,column=1,sticky=W)
+
+        print(self.renovarC_str)
+        print(int(self.renovarC_str))
+
+        value=StringVar(self.renovarC_str)
+
+        self.CMBRenovarC = ttk.Combobox(self.marco,textvariable=value,state ='readonly')
+        self.CMBRenovarC['values'] = ['False', 'True']
+        self.CMBRenovarC.current(int(self.renovarC_str))
+        self.CMBRenovarC.grid(row=5,column=1,sticky=W)
+
+
+        #self.chk3 = Checkbutton(self.marco,variable=self.renovarC_str)
+        #self.chk3.grid(row=5,column=1,sticky=W)
         #self.TXTRenovarC = ttk.Entry(self.marco,width=30)
         #self.TXTRenovarC.insert(0,renovarC_str)
         #self.TXTRenovarC.grid(row=5,column=1)
@@ -97,11 +133,11 @@ class VentanaConfig():
         #row 7 : Tercera fila
         self.LBLDelaysC = ttk.Label(self.marco,text="Delays renovacion circuito:")
         self.LBLDelaysC.grid(row=6,column=0,sticky=W)
-      #  delaysC_str = self.configuracion.getDelayIntentoRenovacionCircuitoTor()
-      #  self.TXTDelaysC = ttk.Entry(self.marco,width=30)
-      #  self.TXTDelaysC.insert(0,delaysC_str)
+        delaysC_str = self.configuracion.getDelayIntentoRenovacionCircuitoTor()
+        #self.TXTDelaysC = ttk.Entry(self.marco,width=30)
+        #self.TXTDelaysC.insert(0,delaysC_str)
 
-        self.TXTDelaysC = Scale(self.marco, from_=1, to=200, orient=HORIZONTAL)
+        self.TXTDelaysC = Scale(self.marco, from_=delaysC_str, to=200, orient=HORIZONTAL)
         self.TXTDelaysC.grid(row=6,column=1,sticky=W)
 
         self.separador1= ttk.Separator(self.marco,orient=HORIZONTAL)
@@ -112,49 +148,24 @@ class VentanaConfig():
         self.BTN1.grid(row=9,column=0)
 
 
+        #self.BTNGuardar = ttk.Button(self.ventana, text = 'Guardar', command = self.guardarConfig)
+        #self.BTNGuardar.pack(side = LEFT)
 
-
-
-
-
-
-
-
-        #Creamos una caja de texto
-      #  self.TXTRutaSalida = Text(self.ventana, width=40, height = 1)
-      #  self.TXTRutaSalida.pack(side=TOP)
-        #Cargamos los datos de la configuracion
-      #  self.TXTRutaSalida.insert("1.0",self.configuracion.getRutaSalida())
-
-      #  self.LBLRutaSalida = Tk.Label(self.ventana, text="Ruta de salida", width=30)
-
-
-
-
-
-
-
-
-       # self.BTNGuardar = ttk.Button(self.ventana, text = 'Guardar', command = self.guardarConfig)
-       # self.BTNGuardar.pack(side = LEFT)
-
-     #   self.BTNSalir =ttk.Button(self.ventana, text='Salir',command=self.ventana.destroy)
-     #   self.BTNSalir.pack(side=RIGHT)
+        #self.BTNSalir =ttk.Button(self.ventana, text='Salir',command=self.ventana.destroy)
+        #self.BTNSalir.pack(side=RIGHT)
 
         #Resaltamos el borde del botoninfo
-     #   self.BTNGuardar.focus_set()
+        #self.BTNGuardar.focus_set()
         self.ventana.mainloop()
 
 
 
- #   def guardarConfig(self):
+        #def guardarConfig(self):
         #Borrar contenido de la caja de texto
-      #  self.TXTRutaSalida.delete("1.0",END)
+        #self.TXTRutaSalida.delete("1.0",END)
 
         #Contruimos una cadena de texto con toda la info y la insertamos en la cadena de texto creada
 
-      #  configuracion = config ()
-    # info = configuracion.toJSON()
-    #    self.TXTRutaSalida.insert("1.0",info)
-
-
+        #configuracion = config ()
+        #info = configuracion.toJSON()
+        #self.TXTRutaSalida.insert("1.0",info)
