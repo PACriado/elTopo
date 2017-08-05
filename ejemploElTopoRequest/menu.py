042f6bf09e5cc2cdbe510e482676a60653c6e7d1
rebeca <rebecaglez22@gmail.com>
2017-08-06 00:28:11 +0200

    Script con la parte crawler local y remoto funcionando

--- ejemploElTopoRequest/menu.py
+++ ejemploElTopoRequest/menu.py
@@ -1,12 +1,19 @@
-import sys, os
-from interfazGrafica.VentanaConfig import VentanaConfig
-from interfazGrafica.VentanaConfigCrawler import VentanaConfigCrawler
-from interfazGrafica.VentanaCrawler import VentanaCrawler
-from interfazGrafica.VentanaClasificador import VentanaClasificador
-from interfazGrafica.VentanaEntrenamiento import VentanaEntrenamiento
+import sys, os, time
+from spyder.spyder import spyder
+from clasificador.classificator import classificator
+from spyderRestCaller.SpyderRestCaller import SpyderRestCaller
+from EntidadesRest.SpyderRequest import SpyderRequest
+from preProcesador.preProcesator import Preprocesator
+from configElTopo.config import config
+from spyderRest import spyderRest
 
 acciones_menu = {}
 acciones_submenu = {}
+acciones_submenuConfig = {}
+acciones_submenuCrawler = {}
+acciones_submenuCrawlerR = {}
+boolSpyder = False
+
 
 def menu_principal():
     os.system('clear')
@@ -18,12 +25,15 @@ def menu_principal():
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     print("\n")
     print("1. Configuracion")
-    print("2. Crawler")
-    print("3. Clasificador")
+    print("2. Crawler Local")
+    print("3. Crawler Remoto")
+    print("4. Clasificador")
     print("\n0. Salir")
     eleccion = input(" >>  ")
+    print(eleccion)
     exec_menu(eleccion)
     return
+
 
 def sub_menuConfig():
     os.system('clear')
@@ -33,6 +43,8 @@ def sub_menuConfig():
     print("0. Salir")
     eleccion = input(" >>  ")
     exec_submenuConfig(eleccion)
+    return
+
 
 def sub_menuCrawler():
     os.system('clear')
@@ -42,6 +54,8 @@ def sub_menuCrawler():
     print("0. Salir")
     eleccion = input(" >>  ")
     exec_submenuCrawler(eleccion)
+    return
+
 
 def sub_menuCrawlerR():
     os.system('clear')
@@ -51,13 +65,14 @@ def sub_menuCrawlerR():
     print("0. Salir")
     eleccion = input(" >>  ")
     exec_submenuCrawlerR(eleccion)
+    return
+
 
 def exec_menu(eleccion):
     os.system('clear')
     ch = eleccion.lower()
     if ch == '':
         acciones_menu['main_menu']()
-
     else:
         try:
             acciones_menu[ch]()
@@ -66,32 +81,47 @@ def exec_menu(eleccion):
             acciones_menu['main_menu']()
     return
 
+
 def exec_submenuConfig(eleccion):
     os.system('clear')
     ch = eleccion.lower()
-    print("hey")
     if ch == '':
         acciones_submenuConfig[ch]()
-        print("hey2")
     else:
         try:
-            acciones_submenuConfig[ch]
-            print("hey3")
+            acciones_submenuConfig[ch]()
         except KeyError:
             print("Opcion no válida.Seleccionar otra opcion.\n")
             acciones_submenuConfig['sub_menuConfig']()
+
 
 def exec_submenuCrawler(eleccion):
     os.system('clear')
     ch = eleccion.lower()
     if ch == '':
-        acciones_submenuCrawler
+        acciones_submenuCrawler[ch]()
+    else:
+        try:
+            acciones_submenuCrawler[ch]()
+        except KeyError:
+            print("Opcion no válida.Seleccionar otra opcion.\n")
+            acciones_submenuCrawler['sub_menuCrawler']()
+
+
+
 
 def exec_submenuCrawlerR(eleccion):
     os.system('clear')
     ch = eleccion.lower()
     if ch == '':
-        acciones_submenuCrawlerR
+            acciones_submenuCrawlerR[ch]()
+    else:
+        try:
+                acciones_submenuCrawlerR[ch]()
+        except KeyError:
+            print("Opcion no válida.Seleccionar otra opcion.\n")
+            acciones_submenuCrawlerR['sub_menuCrawlerR']()
+
 
 def Configuracion():
     print("Menu Configuracion\n")
@@ -99,29 +129,29 @@ def Configuracion():
     print("9. Volver")
     print("0. Salir")
     eleccion = input(" >>  ")
+    print(eleccion)
     exec_submenuConfig(eleccion)
-    return
 
 
-def Crawler():
+def CrawlerL():
     print("Menu Crawler Local\n")
-    print("1. Configurar")
-    print("2. Abrir")
+    print("1. Iniciar")
     print("9. Volver")
     print("0. Salir")
     eleccion = input(" >>  ")
     exec_submenuCrawler(eleccion)
     return
 
+
 def CrawlerR():
     print("Menu Crawler Remoto\n")
-    print("1. Configurar")
-    print("2. Abrir")
+    print("1. Iniciar Crawler Remoto")
     print("9. Volver")
     print("0. Salir")
     eleccion = input(" >>  ")
     exec_submenuCrawlerR(eleccion)
     return
+
 
 def Clasificador():
     print("Menu Clasificador\n")
@@ -132,31 +162,53 @@ def Clasificador():
     exec_menu(eleccion)
     return
 
+
 def Configurar():
     print("Editar el fichero.json")
+    time.sleep(5)
+    menu_principal()
+
 
 def IniciarCrawlerL():
     print("Iniciando crawler local")
+    configuracion = config("./configElTopo/config.json")
+    theSpyder = spyder(rutaConfig="./configElTopo/config.json")
+    rutaSalidaSpyder = theSpyder.launch()
+    procesador = Preprocesator(rutaSalidaSpyder + "onLine/", configuracion.getRutaSalidaPreProcesador())
+    procesador.process()
+    time.sleep(7)
+    menu_principal()
+
 
 def IniciarCrawlerR():
     print("Iniciando crawler remoto")
+    caller = SpyderRestCaller()
+    paths = caller.callList()
+    print(paths)
+    time.sleep(15)
+    menu_principal()
+
 
 def Clasificador():
     print("Abrir")
     print("Entrenar")
-    #Entrenar
+    # Entrenar
+
 
 def back():
     acciones_menu['main_menu']()
 
+
 def exit():
     sys.exit()
+
 
 acciones_menu = {
     'main_menu': menu_principal,
     '1': Configuracion,
-    '2': Crawler,
-    '3': Clasificador,
+    '2': CrawlerL,
+    '3': CrawlerR,
+    '4': Clasificador,
     '9': back,
     '0': exit,
 }
@@ -168,7 +220,7 @@ acciones_submenuConfig = {
     '0': exit
 }
 
-acciones_submenuCrawler ={
+acciones_submenuCrawler = {
     'sub_menuCrawler': sub_menuCrawler,
     '1': IniciarCrawlerL,
     '9': back,
@@ -181,7 +233,6 @@ acciones_submenuCrawlerR = {
     '9': back,
     '0': exit
 }
-
 
 if __name__ == "__main__":
     menu_principal()
