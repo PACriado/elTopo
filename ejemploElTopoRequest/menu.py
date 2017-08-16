91974d5dfb2d4e4415a706fb0a6af8997dc4ccb6
rebeca <rebecaglez22@gmail.com>
2017-08-16 20:42:24 +0200

    Script - Añadidos menus Clasificador y Entrenador

--- ejemploElTopoRequest/menu.py
+++ ejemploElTopoRequest/menu.py
@@ -8,6 +8,8 @@ from configElTopo.config import config
 from spyderRest import spyderRest
 from random import randint
 from asciimatics.screen import Screen
+from clasificador.dataClasificator import dataClasificator
+from Entidades.webPageInfo import webPageInfo
 
 acciones_menu = {}
 acciones_submenu = {}
@@ -15,28 +17,38 @@ acciones_submenuConfig = {}
 acciones_submenuCrawler = {}
 acciones_submenuCrawlerR = {}
 boolSpyder = False
+configuracion = config("./configElTopo/config.json")
+ficheroParaEntrenamiento = configuracion.getFicheroParaEntrenamiento()
+ficheroParaEntrenamientoGeneradoParaEntrenamiento = configuracion.getFicheroParaEntrenamientoGeneradoParaEntrenamiento()
+classificatorObject = classificator(ficheroParaEntrenamiento, configuracion.getRutaFicheroEntrenamientoPersistente(),
+                                    configuracion.getRutaJSONTraining(),
+                                    ficheroParaEntrenamientoGeneradoParaEntrenamiento)
+classificatorObject = classificator(ficheroParaEntrenamiento,
+                                    configuracion.getRutaFicheroEntrenamientoPersistente(),
+                                    configuracion.getRutaSalidaPreProcesador(),
+                                    ficheroParaEntrenamientoGeneradoParaEntrenamiento)
 
+classifier = classificatorObject.getClasifier()
+webPageInfoObject = webPageInfo(route='/home/usertfm/SalidaJSON/Preproc/google.es.json')
+miClass = dataClasificator(webPageInfoObject, classifier)
 
 
 def letrasInicio(screen):
     x = 0
-    while x<1000:
+    while x < 1000:
         screen.print_at('El topo',
                         randint(0, screen.width), randint(0, screen.height),
                         colour=randint(0, screen.colours - 1),
                         bg=randint(0, screen.colours - 1))
         ev = screen.get_key()
         screen.refresh()
-        x+=1
-
-
+        x += 1
 
 
 def menu_principal():
     os.system('clear')
 
     Screen.wrapper(letrasInicio)
-
 
     print("===============================")
     print(" MENU PRINCIPAL: EL TOPO       ")
@@ -47,7 +59,8 @@ def menu_principal():
     print("1. Configuracion")
     print("2. Crawler Local")
     print("3. Crawler Remoto")
-    print("4. Clasificador")
+    print("4. Entrenamiento")
+    print("5. Clasificador")
     print("\n0. Salir")
     eleccion = input(" >>  ")
     print(eleccion)
@@ -86,6 +99,11 @@ def sub_menuCrawlerR():
     eleccion = input(" >>  ")
     exec_submenuCrawlerR(eleccion)
     return
+
+
+def sub_menuClasificador():
+    os.system('clear')
+    print("Menu Clasificador")
 
 
 def exec_menu(eleccion):
@@ -128,19 +146,30 @@ def exec_submenuCrawler(eleccion):
             acciones_submenuCrawler['sub_menuCrawler']()
 
 
-
-
 def exec_submenuCrawlerR(eleccion):
     os.system('clear')
     ch = eleccion.lower()
     if ch == '':
-            acciones_submenuCrawlerR[ch]()
+        acciones_submenuCrawlerR[ch]()
     else:
         try:
-                acciones_submenuCrawlerR[ch]()
+            acciones_submenuCrawlerR[ch]()
         except KeyError:
             print("Opcion no válida.Seleccionar otra opcion.\n")
             acciones_submenuCrawlerR['sub_menuCrawlerR']()
+
+
+def exec_submenuClasificador(eleccion):
+    os.system('clear')
+    ch = eleccion.lower()
+    if ch == '':
+        acciones_submenuClasificador[ch]()
+    else:
+        try:
+            acciones_submenuClasificador[ch]()
+        except KeyError:
+            print("Opcion no válida.Seleccionar otra opcion.\n")
+            acciones_submenuClasificador['sub_menuClasificador']()
 
 
 def Configuracion():
@@ -175,11 +204,18 @@ def CrawlerR():
 
 def Clasificador():
     print("Menu Clasificador\n")
-    print("1. Clasificar")
+    print("1. All data")
+    print("2. Title data ")
+    print("3. Span data")
+    print("4. Meta data")
+    print("5. Paragraph data")
+    print("6. Url data")
+    print("7. Header data")
     print("9. Volver")
     print("0. Salir")
     eleccion = input(" >>  ")
-    exec_menu(eleccion)
+
+    exec_submenuClasificador(eleccion)
     return
 
 
@@ -191,7 +227,6 @@ def Configurar():
 
 def IniciarCrawlerL():
     print("Iniciando crawler local")
-    configuracion = config("./configElTopo/config.json")
     theSpyder = spyder(rutaConfig="./configElTopo/config.json")
     rutaSalidaSpyder = theSpyder.launch()
     procesador = Preprocesator(rutaSalidaSpyder + "onLine/", configuracion.getRutaSalidaPreProcesador())
@@ -209,11 +244,46 @@ def IniciarCrawlerR():
     menu_principal()
 
 
-def Clasificador():
-    print("Abrir")
-    print("Entrenar")
-    # Entrenar
+def Entrenador():
+    # Menu de botones
 
+    print("Iniciando Entrenamiento...")
+    time.sleep(3)
+    menu_principal()
+    # Llamada al método de entrenamiento
+
+
+def Alldata():
+    print(miClass.classifyAllHeaders())
+    time.sleep(3)
+    sub_menuClasificador()
+
+def Titledata():
+    print(miClass.classifyAllTitles())
+    time.sleep(3)
+sub_menuClasificador()
+def Spandata():
+    print(miClass.classifyAllSpan())
+    time.sleep(3)
+sub_menuClasificador()
+def Metadata():
+    print(miClass.classifyAllMeta())
+    time.sleep(3)
+    sub_menuClasificador()
+
+def Paragraphdata():
+    print(miClass.classifyAllParrafos())
+    time.sleep(3)
+sub_menuClasificador()
+
+def Urldata():
+    print(miClass.classifyAllUrl())
+    time.sleep(3)
+sub_menuClasificador()
+def Headerdata():
+    print(miClass.classifyAllHeaders())
+    time.sleep(3)
+    sub_menuClasificador()
 
 def back():
     acciones_menu['main_menu']()
@@ -228,7 +298,8 @@ acciones_menu = {
     '1': Configuracion,
     '2': CrawlerL,
     '3': CrawlerR,
-    '4': Clasificador,
+    '4': Entrenador,
+    '5': Clasificador,
     '9': back,
     '0': exit,
 }
@@ -250,6 +321,17 @@ acciones_submenuCrawler = {
 acciones_submenuCrawlerR = {
     'sub_menuCrawlerR': sub_menuCrawlerR,
     '1': IniciarCrawlerR,
+    '9': back,
+    '0': exit
+}
+acciones_submenuClasificador = {
+    'sub_menuClasificador': sub_menuClasificador,
+    '1': Alldata,
+    '2': Titledata,
+    '3': Metadata,
+    '4': Spandata,
+    '5': Urldata,
+    '6': Headerdata,
     '9': back,
     '0': exit
 }
